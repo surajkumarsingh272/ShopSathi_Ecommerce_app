cb96edfe34366fa9c0836015af31543e31480825
Vikram Kumar <vikramkumar6202856199@gmail.com>
2025-12-10 11:14:03 +0530

    sqflite remove change delivery screen

--- lib/Change_Delivery_Address/address_api_service.dart
+++ lib/Change_Delivery_Address/address_api_service.dart
@@ -14,7 +14,6 @@ class AddressApiService {
     return null;
   }
 
-
   static Future<AddressModel?> postAddressApiData(Map<String,dynamic> data)async{
     var response = await http.post(Uri.parse("http://192.168.31.190:2001/add_address"),headers: {"Content-Type":"application/json"},body: jsonEncode(data));
     if(response.statusCode == 200 || response.statusCode == 201){
